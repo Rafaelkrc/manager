2b3a7e796d26ed71db5fdfe37bddb991ba834bc3
Rafaelkrc <rafaelkrc@hotmail.com>
2024-06-11 20:36:34 +0100

    created production movements list
    close #42

--- app/urls.py
+++ app/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('', include('employees.urls')),
     path('', include('products.urls')),
     path('', include('dispatch_production.urls')),
+    path('', include('production_movements.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
