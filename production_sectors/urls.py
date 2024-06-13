4f9f69e1f842a35c07febf4b530bc98d9e660f9b
Rafaelkrc <rafaelkrc@hotmail.com>
2024-06-13 16:42:36 +0100

    created production sector register
    close #50

--- production_sectors/urls.py
+++ production_sectors/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('production_setor/list/', views.ProductionSectorListView.as_view(), name='production_sector_list')
+    path('production_setor/list/', views.ProductionSectorListView.as_view(), name='production_sector_list'),
+    path('production_setor/create/', views.ProductionSectorCreateView.as_view(), name='production_sector_create'),
 ]
