90b4ba264096196f31656af2f73c1a27c0597a70
Rafaelkrc <rafaelkrc@hotmail.com>
2024-06-19 21:12:10 +0100

    created dispatch production templates
    close #71

--- dispatch_production/urls.py
+++ dispatch_production/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('dispatch_production/list/', views.DispatchProductionListView.as_view(), name='dispatch_production_list'),
     path('dispatch_production/create/', views.DispatchProductionCreateView.as_view(), name='dispatch_production_create'),
+    path('dispatch_production/<int:pk>/detail', views.DispatchProductionDetailView.as_view(), name='dispatch_production_detail'),
 ]
