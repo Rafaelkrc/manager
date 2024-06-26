d0c8388ec873cab81c847cfab3a5fac920850d15
Rafaelkrc <rafaelkrc@hotmail.com>
2024-06-26 11:27:39 +0100

    created subsidiary update and delete
    close #85

--- subsidiaries/urls.py
+++ subsidiaries/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('subsidiaries/list/', views.SubsidiaryListView.as_view(), name='subsidiary_list'),
     path('subsidiaries/create/', views.SubsidiaryCreateView.as_view(), name='subsidiary_create'),
     path('subsidiaries/<int:pk>/detail/', views.SubsidiaryDetailView.as_view(), name='subsidiary_detail'),
+    path('subsidiaries/<int:pk>/update/', views.SubsidiaryUpdateView.as_view(), name='subsidiary_update'),
+    path('subsidiaries/<int:pk>/delete/', views.SubsidiaryDeleteView.as_view(), name='subsidiary_delete'),
 ]
