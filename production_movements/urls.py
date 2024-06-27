044f51c40b9600576fb3851734ce9088f0245958
Rafaelkrc <rafaelkrc@hotmail.com>
2024-06-27 17:42:52 +0100

    created production movement update and delete
    close #91

--- production_movements/urls.py
+++ production_movements/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('production_movement/list/', views.ProductionMovementListView.as_view(), name='production_movement_list'),
     path('production_movement/create/', views.ProductionMovementCreateView.as_view(), name='production_movement_create'),
     path('production_movement/<int:pk>/detail/', views.ProductionMovementDetailView.as_view(), name='production_movement_detail'),
+    path('production_movement/<int:pk>/update/', views.ProductionMovementUpdateView.as_view(), name='production_movement_update'),
+    path('production_movement/<int:pk>/delete/', views.ProductionMovementDeleteView.as_view(), name='production_movement_delete'),
 ]
