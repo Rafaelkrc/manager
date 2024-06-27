deed91328825c450a669ab6a766341f161f8db18
Rafaelkrc <rafaelkrc@hotmail.com>
2024-06-27 19:05:14 +0100

    created product update and delete
    close #92

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, CreateView, DetailView
+from django.contrib import messages
+from django.db.models import ProtectedError
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from . import models, forms
 
@@ -36,3 +39,35 @@ class ProductDetailView(DetailView):
     model = models.Product
     template_name = 'product_detail.html'
     context_object_name = 'product_details'
+
+
+class ProductUpdateView(UpdateView):
+    model = models.Product
+    form_class = forms.ProductForm
+    template_name = 'product_update.html'
+    success_url = reverse_lazy('product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = models.Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('product_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.get_object()
+        context['code'] = product.code
+        context['name'] = product.name
+        context['group'] = product.group
+        context['brand'] = product.brand
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data()
+        try:
+            self.object.delete()
+            return redirect(self.success_url)
+        except ProtectedError:
+            messages.error(request, "Product linked to a movement, cannot be deleted!")
+            return render(request, 'product_undelete.html', context)
